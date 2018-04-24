c5875d2758b67e8a4e0877b049d4899294a70a33
Longbin Chen <longbin.chen@glassdoor.com>
2018-04-24 09:50:18 -0700

    load model fix

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXX = g++
 CXXFLAGS = -Wall -O3 -std=c++0x -march=native
 
 # comment the following flags if you do not want to SSE instructions
-DFLAG += -DUSESSE
+#DFLAG += -DUSESSE
 
 # comment the following flags if you do not want to use OpenMP
 #DFLAG += -DUSEOMP
@@ -17,7 +17,7 @@ ffm-predict: ffm-predict.cpp ffm.o timer.o
 	$(CXX) $(CXXFLAGS) $(DFLAG) -o $@ $^
 
 ffm-convert: ffm-convert.cpp ffm.o timer.o
-	$(CXX) $(CXXFLAGS)  -o $@ $^
+	$(CXX) $(CXXFLAGS) $(DFLAG) -o $@ $^
 
 ffm.o: ffm.cpp ffm.h timer.o
 	$(CXX) $(CXXFLAGS) $(DFLAG) -c -o $@ $<
--- ffm-convert.cpp
+++ ffm-convert.cpp
@@ -8,7 +8,7 @@
 #include <stdexcept>
 #include <vector>
 #include <cstdlib>
-
+#include <xmmintrin.h>
 #include "ffm.h"
 
 using namespace std;
@@ -16,11 +16,13 @@ using namespace ffm;
 
 struct Option {
     string  model_path, output_path;
+    int field_id;
 };
 
 string convert_help() {
     return string(
-"usage: ffm-convert model_file output_file\n");
+"usage: ffm-convert model_file output_file field_id\n"
+);
 }
 
 Option parse_option(int argc, char **argv) {
@@ -33,18 +35,20 @@ Option parse_option(int argc, char **argv) {
 
     Option option;
 
-    if(argc != 3)
+    if(argc != 4)
         throw invalid_argument("cannot parse argument");
 
     option.model_path = string(args[1]);
     option.output_path = string(args[2]);
+    option.field_id = atoi(args[3].c_str());
+    
 
     return option;
 }
 
 
-
-void convert(string model_path, string output_path) {
+#if defined USESSE
+void convert(string model_path, string output_path, int field_id) {
     ofstream f_out(output_path);
     ffm_model model = ffm_load_model(model_path);
     ffm_int align0 = 2 * get_k_aligned(model.k);
@@ -55,17 +59,49 @@ void convert(string model_path, string output_path) {
     cout << "field # " << model.m << endl;
     cout << "vector dimension (k) # " << model.k << endl;
     cout << "kALIGN " << kALIGN << endl;
+
+
     for(int f = 0; f < model.n ; f ++){
-        for (int j = 0; j < model.m; j ++) {
-            f_out << f << '\t' << j;
-            ffm_float *w = model.W + (ffm_long)f*align1 + j*align0;
-            for(ffm_int d = 0; d < align0; d += kALIGN * 2){
-                f_out  << '\t' << w[d];
+        int j = field_id;
+        f_out << f << '\t' << j;
+        ffm_float *w = model.W + (ffm_long)f*align1 + j*align0;
+        for(ffm_int d = 0; d < align0; d += kALIGN * 2){
+            __m128  XMMw1 = _mm_load_ps(w+d);
+            float result[4];
+            _mm_store_ps(result, XMMw1);
+            for(int kk = 0; kk < 4; kk ++) {
+                f_out  << '\t' << result[kk];
             }
-            f_out << endl;
         }
+        f_out << endl;
     }
 }
+
+#else
+void convert(string model_path, string output_path, int field_id) {
+    ofstream f_out(output_path);
+    ffm_model model = ffm_load_model(model_path);
+    ffm_int align0 = 2 * get_k_aligned(model.k);
+    ffm_int align1 = model.m * align0;
+    cout << "align 0 (size per fields per feature)" << align0 << endl;
+    cout << "align 1 (size per feature)" << align1 << endl;
+    cout << "feature # " << model.n << endl;
+    cout << "field # " << model.m << endl;
+    cout << "vector dimension (k) # " << model.k << endl;
+    cout << "kALIGN " << kALIGN << endl;
+
+
+    for(int f = 0; f < model.n ; f ++){
+        int j = field_id;
+        f_out << f << '\t' << j;
+        ffm_float *w = model.W + (ffm_long)f*align1 + j*align0;
+        for(ffm_int d = 0; d < align0; d += kALIGN * 2){
+            f_out  << '\t' << w[d];
+        }
+        f_out << endl;
+    }
+}
+#endif
 
 int main(int argc, char **argv) {
     Option option;
@@ -76,7 +112,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    convert(option.model_path, option.output_path);
+    convert(option.model_path, option.output_path, option.field_id);
 
     return 0;
 }
